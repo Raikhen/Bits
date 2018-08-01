fdcbb220b71f695695246e056dcc42229bc61d1f
Dylan Fridman <dylanfridman@gmail.com>
2018-08-01 11:08:09 -0300

    Added Icon support to the Button component

--- components/Button.js
+++ components/Button.js
@@ -4,16 +4,41 @@ import { StyleSheet }               from 'react-native';
 import { TextInput }                from 'react-native';
 import { View }                     from 'react-native';
 import { Text }                     from 'react-native';
+import { Ionicons }                 from '@expo/vector-icons';
+import { MaterialIcons }            from '@expo/vector-icons';
+import { MaterialCommunityIcons }   from '@expo/vector-icons';
 
 import Constants                    from '../utilities/Constants';
 
 export default class Button extends React.Component {
+  renderIcon() {
+    const { props } = this;
+
+    if (props.icon) {
+      let iconSize;
+
+      if (props.iconSize) iconSize = props.iconSize;
+      else iconSize = 24;
+
+      return (
+        <MaterialCommunityIcons
+          style={[styles.icon, props.iconStyle]}
+          name={props.icon}
+          color={Constants.styling.colors.primary}
+          size={iconSize} />
+      );
+    }
+
+    return null;
+  }
+
   render() {
     return (
       <TouchableWithoutFeedback
         onPress={() => this.props.onPress()}
         {...this.props}>
         <View style={styles.container}>
+          {this.renderIcon()}
           <Text style={styles.text}>
             {this.props.text}
           </Text>
@@ -30,10 +55,14 @@ const styles = StyleSheet.create({
     paddingRight: 15,
     paddingLeft: 15,
     padding: 10,
-    margin: 20
+    margin: 20,
+    flexDirection: 'row'
   },
   text: {
     ...Constants.styling.text,
     fontSize: 18
+  },
+  icon: {
+    marginRight: 8
   }
 });
--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -5,13 +5,25 @@ import Constants                    from '../../utilities/Constants';
 import Backend                      from '../../backend/Backend';
 import Screen                       from '../../components/Screen';
 import BitsText                     from '../../components/BitsText';
+import Button                       from '../../components/Button';
 
 export default class LoginScreen extends React.Component {
   render() {
     return (
-      <Screen>
+      <Screen style={styles.container}>
         <BitsText>LoginScreen</BitsText>
+        <Button
+          icon="facebook"
+          text="Login with Facebook"
+          onPress={() => console.log('Tried to log in.')} />
       </Screen>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
